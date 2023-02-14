2d3c251aa22c6ad09a85810b4250ad6526e1c480
Donggyeong Yi <realldk@naver.com>
2023-02-14 14:01:43 +0900

    설명 수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,8 @@
 import { Group } from './learn/react.components.js';
 
 // 함수 타입
-// React 월드
-// React 엘리먼트 생성 과정에서는 반드시 React.createElement API
+// React 월드에서는
+// React 엘리먼트 생성 과정에서는 반드시 React.createElement API를 써야한다.
 
 // 사용법 1
 // 웹브라우저가 인식 가능한 HTML 표준 엘리먼트 이름 (문자 값)
