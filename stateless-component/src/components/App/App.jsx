91409b5e7c3a1d72d43af5e5ec27cc8de95d1338
Donggyeong Yi <realldk@naver.com>
2023-02-23 10:00:50 +0900

    learn 상태가 없는 컴포넌트 설계 A11yHidden

--- stateless-component/src/components/App/App.jsx
+++ stateless-component/src/components/App/App.jsx
@@ -9,10 +9,15 @@ function App() {
       <div role="group" className={classes.buttonGroup}>
         <Button>회원가입</Button>
         <Button secondary>로그인</Button>
-
-        <A11yHidden as="h2">저장</A11yHidden>
-        <A11yHidden as="a">저장</A11yHidden>
-        <A11yHidden as="figcaption">저장</A11yHidden>
+        <section>
+          <A11yHidden as='h2' className="myClassName">접근성 준수한 제목</A11yHidden>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error similique unde vitae? Quibusdam esse voluptates similique odit facere, animi optio iste explicabo pariatur a omnis dignissimos itaque deserunt ea voluptate.</p>
+          <p>Beatae voluptas rem possimus corrupti, iste, eligendi unde adipisci atque sint temporibus ab, quam aliquam. Repellat ipsam magni officiis consequatur nobis quisquam, nam corrupti non aut praesentium assumenda autem id.</p>
+          <p>Obcaecati ad sed quidem quasi porro ipsum cumque minima saepe quos, doloremque quia earum eligendi architecto facere, beatae fugiat at dicta assumenda ex? Voluptates, consequuntur possimus. Inventore minus ducimus eveniet.</p>
+          <p>Esse asperiores facere omnis iste perferendis. Quas reiciendis perferendis veritatis a debitis architecto, dolores reprehenderit expedita mollitia aliquid non nemo rerum illo voluptas corporis cupiditate consequatur suscipit aperiam perspiciatis minus.</p>
+          <p>Reiciendis suscipit quae repudiandae adipisci, unde quis aspernatur alias voluptatum consectetur ex quia possimus voluptates fugiat soluta, beatae quod voluptatibus quidem! Sunt deleniti officia excepturi velit? Nulla quia dolore voluptates?</p>
+          <p>Expedita fugiat sit non deserunt, quo quae molestiae odit modi repudiandae veniam sint, suscipit quisquam saepe, eum voluptatem facilis quasi consequatur minima quidem. Ex, nulla? Labore inventore maiores doloremque quos.</p>
+        </section>
       </div>
     </div>
   );
