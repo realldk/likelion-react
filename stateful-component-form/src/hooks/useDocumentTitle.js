7ddeb061df463ce9a5e4b49637978edb3fb17b5c
Donggyeong Yi <realldk@naver.com>
2023-02-25 23:09:51 +0900

    임시

---
+++ stateful-component-form/src/hooks/useDocumentTitle.js
@@ -0,0 +1,12 @@
+import { useLayoutEffect } from 'react';
+
+// 사용자 저의 훅
+// use 접두사를 사용하는 함수
+// 웹 문서의 제목을 변경하는 훅
+// 사이드 이펙트다
+
+export const useDocumentTitle = (titleContent) => {
+  useLayoutEffect(() => {
+    document.title = titleContent;
+  })
+}
