70257c6965c93a4c2b44e32749ae234d5d5880a1
Donggyeong Yi <realldk@naver.com>
2023-02-26 13:08:35 +0900

    FormInput  컴포넌트 설계중

--- stateless-component/src/components/index.js
+++ stateless-component/src/components/index.js
@@ -3,3 +3,4 @@ export * from './A11yHidden/A11yHidden';
 export * from './Nav/Nav';
 export * from './Layout/Layout';
 export * from './SkipToContent/SkipToContent'
+export * from './FormInput/FormInput'
